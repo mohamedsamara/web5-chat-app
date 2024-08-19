c6b4164a85583359b9723c96765a84127dc4ff06
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2024-08-18 20:12:40 -0700

    chore: fix lint errors

--- src/pages/Chat/components/AudioRecordingProgress.tsx
+++ src/pages/Chat/components/AudioRecordingProgress.tsx
@@ -1,29 +1,14 @@
-// @ts-ignore
-import WaveSurfer from "wavesurfer.js";
-
 import RecordingTimer from "./RecordingTimer";
-import { useDimension } from "@/lib/hooks";
-import { useRef } from "react";
 
 type Props = {
   mediaRecorder: MediaRecorder;
   duration: number;
 };
 
-const AudioRecordingProgress = ({ mediaRecorder, duration }: Props) => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const { width } = useDimension(ref);
-
+const AudioRecordingProgress = ({ duration }: Props) => {
   return (
     <div className="flex items-center gap-4">
       <RecordingTimer duration={duration} />
-      <div ref={ref} className="flex-1">
-        {/* <LiveAudioVisualizer
-          mediaRecorder={mediaRecorder}
-          width={width - 50}
-          height={40}
-        /> */}
-      </div>
     </div>
   );
 };
