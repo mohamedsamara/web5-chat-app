15922361f8ff48ca9f7640f9f5c1c1d98b7aa511
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2024-11-21 09:20:58 -0800

    chore: remove unnecessary comment

--- src/lib/utils/app.ts
+++ src/lib/utils/app.ts
@@ -324,7 +324,6 @@ export const segregateMsgText = (text: string) => {
 };
 
 export const isDid = (str: string) => {
-  // TODO: Need to check kif there is an existing utlity
   const didStr = str.split(":");
   const identifier = didStr[2];
   return str.startsWith("did:dht") && identifier.length === 52;
